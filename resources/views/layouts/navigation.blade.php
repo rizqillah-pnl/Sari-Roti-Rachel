7df1f7275e96e2fc424923a07450b60d3a2ca7f4
Rachel Ardana Putra Ginting <rachelardanaputraginting@gmail.com>
2022-12-02 03:38:38 +0700

    Membuat event click pada notifikasi

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -65,20 +65,38 @@
                             {{ $order }}</div>
                     </a>
                 @endif
-                @if (Auth::user()->member === 3 || Auth::user()->member === 2)
+                @if (Auth::user()->member === 3)
                     <label for="my-modal-2"
-                        class="text-lg hover:rounded-lg text-primary {{ Route::is('/') ? 'font-bold text-primary' : '' }} relative"><svg
-                            xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell mr-6"
-                            width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        class="block text-lg hover:rounded-lg text-primary {{ Route::is('/') ? 'font-bold text-primary' : '' }} relative animate-bounce duration-200"
+                        onclick="notifFunction()" id="label"><svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-bell mr-6" width="28" height="28"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path
                                 d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6">
                             </path>
                             <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
                         </svg>
-                        <div
-                            class="badge badge-sm border-none text-white py-2.5  bg-secondary border border-secondary shadow-lg absolute top-0 right-4 text-xs">
+                        <div class="badge badge-sm border-none text-white py-2.5  bg-secondary border border-secondary shadow-lg absolute top-0 right-4 text-xs"
+                            id="notif">
+                            1</div>
+                    </label>
+                @elseif(Auth::user()->member === 2)
+                    <label for="my-modal-2"
+                        class="text-lg hover:rounded-lg text-primary {{ Route::is('/') ? 'font-bold text-primary' : '' }} relative animate-bounce duration-200"
+                        onclick="notifFunction()" id="label"><svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-bell mr-6" width="28" height="28"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path
+                                d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6">
+                            </path>
+                            <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
+                        </svg>
+                        <div class="badge badge-sm border-none text-white py-2.5  bg-secondary border border-secondary shadow-lg absolute top-0 right-4 text-xs"
+                            id="notif">
                             1</div>
                     </label>
                 @endif
@@ -162,6 +180,13 @@
         </div>
     @endif
     {{-- akhir modal notifikasi --}}
+
+    <script>
+        function notifFunction() {
+            document.getElementById("notif").remove()
+            document.getElementById("label").classList.remove('animate-bounce')
+        }
+    </script>
 
 </x-container>
 </div>
